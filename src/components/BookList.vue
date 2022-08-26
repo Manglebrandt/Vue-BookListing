0f927cde068c6754fad0834c98244b5a3c78bf44
Mike Anglebrandt <manglebrandt@epfc.com>
2022-08-26 13:39:02 -0400

    sf

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-    <h1>{title}</h1>
+    <h1>{{title}}</h1>
   </div>
 </template>
 
 
 <script>
   export default {
-    name: "BookList"
+    name: 'BookList',
+    data() {
+      return {
+        title: 'All Books'
+      }
+    }
   };
-  function data() { return title = "All Books" };
+  
 </script>
 
 
